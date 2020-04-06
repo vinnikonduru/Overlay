161a297e1e39632cfbb23e79915f5b5b0cebdacc
Vinitha Konduru <vinithak@umn.edu>
2020-04-06 12:37:49 -0500

    Deleted Unused files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ToolLayout from './Components/ToolLayout';
 
 class App extends Component {
   constructor(props) {
@@ -8,7 +7,6 @@ class App extends Component {
   render() {
     return (
       <>
-      <ToolLayout />
       </>
     );
   }
@@ -16,74 +14,3 @@ class App extends Component {
 
 export default App;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { Component } from 'react';
-// // import ToolLayoutTest from './Components/ToolLayoutTest';
-// // import ToolLayoutTest1 from './Components/ToolLayoutTest1';
-// // import ToolTest from './Components/ToolTest';
-// import ToolLayout from './Components/ToolLayout';
-
-// class App extends Component {
-
-//   constructor(props) {
-//     super(props);
-
-//   }
-
-//   render() {
-//     return (
-//       <>
-//         {/* <ToolLayoutTest/> */}
-//         {/* <ToolTest /> */}
-//         {/* <ToolLayoutTest1 /> */}
-//         <ToolLayout />
-
-//       </>
-//     );
-//   }
-// }
-
-// export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const webpack = require('webpack');
-// const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { resolve } = require('path')
 
 
